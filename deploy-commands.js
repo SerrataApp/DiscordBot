8be8d52c7f88d28124b2b0dd3da8ddba22414c4e
TangoCh4rlie <loulou.reyms@orange.fr>
2024-01-22 15:30:53 +0100

    update pipeline

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,10 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+const { CLIENTID, GUILDID, TOKEN } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const commands = [];
 // Grab all the command folders = require( the commands directory you created earlier
@@ -25,7 +28,7 @@ for (const folder of commandFolders) {
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST().setToken(token);
+const rest = new REST().setToken(process.env.TOKEN);
 
 // and deploy your commands!
 (async () => {
@@ -34,7 +37,7 @@ const rest = new REST().setToken(token);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationGuildCommands(process.env.CLIENTID, process.env.GUILDID),
 			{ body: commands },
 		);
 
